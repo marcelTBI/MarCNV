a0b525642d891eaab1fae65cc676237e50209e15
kucharik <marcel.kucharik@geneton.sk>
2022-12-20 12:44:24 +0100

    Added Nomenclature string and CNV size

--- src/components/SearchCNV.tsx
+++ src/components/SearchCNV.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Alert, Link, Paper, Stack, Typography } from '@mui/material'
+import { Alert, Divider, Link, Paper, Stack, Typography } from '@mui/material'
 
 import SubmitButton from '../components/SubmitButton'
 import FormInputText from '../components/forms/FormInputText'
@@ -12,6 +12,7 @@ type FormInput = {
   start: string
   end: string
   cnvType: Option
+  nomenclature: string
 }
 
 export type CNVStats = {
@@ -63,13 +64,20 @@ const validateStart = (location: string) => {
   return true
 }
 
-const validateNomenclature = (nomenclature: string) => {
+const nomenclatureToInterval = (nomenclature: string) => {
   const regexp = new RegExp('^chr(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|X|Y):[,0-9]+-[,0-9]+$')
-  if (!regexp.test(nomenclature)) return 'Nomenclature not in a proper format (chr5:123456789-234567890)'
+  if (!regexp.test(nomenclature)) return
   const [chrom, rest] = nomenclature.split(':')
   const [start, end] = rest.split('-')
-  const startNum = Number(start.replace(',', ''))
-  const endNum = Number(end.replace(',', ''))
+  const startNum = Number(start.replaceAll(',', ''))
+  const endNum = Number(end.replaceAll(',', ''))
+  return [chrom, startNum, endNum] as const
+}
+
+const validateNomenclature = (nomenclature: string) => {
+  const interval = nomenclatureToInterval(nomenclature)
+  if (!interval) return nomenclature === '' || 'Nomenclature not in a proper format (chr5:123456789-234567890)'
+  const [, startNum, endNum] = interval
   if (startNum >= endNum) return 'End location is BEFORE start location!'
   return true
 }
@@ -85,12 +93,46 @@ const validateEnd = (end: string, start: string) => {
 const SearchCNV: React.FC<Props> = ({ submitData }) => {
   const { handleSubmit, control, watch, setValue } = useForm<FormInput>({ defaultValues: {} })
   const locStart = watch('start')
+  const locEnd = watch('end')
+  // const locChrom = watch('chrom')
+  const nomenclature = watch('nomenclature')
 
   const [loading, setLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
 
+  // convert nomenclature to normal interval
+  useEffect(() => {
+    const interval = nomenclatureToInterval(nomenclature)
+    if (interval) {
+      const [chrom, startNum, endNum] = interval
+      setValue('start', startNum.toString())
+      setValue('end', endNum.toString())
+      setValue('chrom', chromosomes.find((value) => value.label === chrom) as Option)
+    }
+  }, [nomenclature, setValue])
+
+  // convert normal interval to nomenclature - does not work due to update cycles
+  /* useEffect(() => {
+    console.log(locStart, locEnd, locChrom)
+    if (locChrom && locStart && locEnd) {
+      setValue('nomenclature', `${locChrom.label}:${locStart.toLocaleString()}-${locEnd.toLocaleString()}`)
+    }
+  }, [locStart, locEnd, locChrom]) */
+
+  const getSize = () => {
+    if (locStart && locEnd && !isNaN(Number(locEnd)) && !isNaN(Number(locStart))) {
+      const lengthCNV = Number(locEnd) - Number(locStart)
+      if (lengthCNV <= 0) return '--- b'
+      if (lengthCNV >= 1000000) return `${(lengthCNV / 1000000).toFixed(0)} Mb`
+      if (lengthCNV >= 1000) return `${(lengthCNV / 1000).toFixed(0)} Kb`
+      return `${lengthCNV} b`
+    }
+    return '--- b'
+  }
+
   const onSubmit = async (data: FormInput) => {
     setLoading(true)
+    setValue('nomenclature', `${data.chrom.label}:${data.start.toLocaleString()}-${data.end.toLocaleString()}`)
     const error = await submitData({
       chrom: data.chrom.label,
       start: Number(data.start),
@@ -103,46 +145,53 @@ const SearchCNV: React.FC<Props> = ({ submitData }) => {
 
   return (
     <Paper sx={{ padding: 2, flex: 1 }}>
-      <Stack spacing={2} alignItems='center'>
+      <Stack spacing={3} alignItems='center'>
         <Typography variant='h5'>Interpretation of copy-number variants</Typography>
         <Stack spacing={2} direction='row'>
-          <FormInputText
-            name='nomenclature'
-            control={control}
-            label='Nomenclature string'
-            disabled={loading}
-            validator={validateNomenclature}
-            sx={{ minWidth: 350 }}
-            placeholder='chr:start-end'
-            required
-          />
-        </Stack>
-        <Stack spacing={2} direction='row' alignItems='center'>
-          <FormInputComboBox name='chrom' control={control} label='Chromosome' disabled={loading} options={chromosomes} sx={{ minWidth: 170 }} required />
-          <Typography>:</Typography>
-          <FormInputText
-            name='start'
-            control={control}
-            label='Location start'
-            inputProps={{ inputMode: 'numeric' }}
-            disabled={loading}
-            validator={validateStart}
-            sx={{ minWidth: 170 }}
-            required
-          />
-          <Typography>-</Typography>
-          <FormInputText
-            name='end'
-            control={control}
-            label='Location end'
-            inputProps={{ inputMode: 'numeric' }}
-            disabled={loading}
-            validator={(value) => validateEnd(value, locStart)}
-            sx={{ minWidth: 170 }}
-            required
-          />
-          <FormInputComboBox name='cnvType' control={control} label='CNV type' disabled={loading} options={cnvTypes} sx={{ minWidth: 170 }} required />
-          <SubmitButton loading={loading} id='login' text='Annotate CNV' onClick={handleSubmit(onSubmit)} size='large' sx={{ minWidth: 220 }} />
+          <Stack spacing={2} alignItems='center'>
+            <Stack spacing={2} direction='row' alignItems='center'>
+              <FormInputText
+                name='nomenclature'
+                control={control}
+                label='Nomenclature string'
+                disabled={loading}
+                validator={validateNomenclature}
+                sx={{ minWidth: 350 }}
+                placeholder='chr:start-end'
+              />
+              <Typography> OR </Typography>
+            </Stack>
+            <Stack spacing={2} direction='row' alignItems='center'>
+              <FormInputComboBox name='chrom' control={control} label='Chromosome' disabled={loading} options={chromosomes} sx={{ minWidth: 170 }} required />
+              <Typography>:</Typography>
+              <FormInputText
+                name='start'
+                control={control}
+                label='Location start'
+                inputProps={{ inputMode: 'numeric' }}
+                disabled={loading}
+                validator={validateStart}
+                sx={{ minWidth: 170 }}
+                required
+              />
+              <Typography>-</Typography>
+              <FormInputText
+                name='end'
+                control={control}
+                label='Location end'
+                inputProps={{ inputMode: 'numeric' }}
+                disabled={loading}
+                validator={(value) => validateEnd(value, locStart)}
+                sx={{ minWidth: 170 }}
+                required
+              />
+            </Stack>
+          </Stack>
+          <Divider orientation='vertical' flexItem />
+          <Stack spacing={2} direction='row' alignItems='center'>
+            <FormInputComboBox name='cnvType' control={control} label='CNV type' disabled={loading} options={cnvTypes} sx={{ minWidth: 170 }} required />
+            <SubmitButton loading={loading} id='login' text={`Annotate (${getSize()})`} onClick={handleSubmit(onSubmit)} size='large' sx={{ minWidth: 235 }} />
+          </Stack>
         </Stack>
         {errorMessage && <Alert severity={'error'}>{errorMessage}</Alert>}
         <Typography align='center' variant='body2'>
